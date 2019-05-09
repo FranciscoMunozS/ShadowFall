b1c3ce50c05feefb420b63b7098bf107ff76f339
Francisco <franciscomuniozs@gmail.com>
2019-05-09 16:38:01 -0400

    Last update - bug fixed

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -69,6 +69,6 @@ class PeriodsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def period_params
-      params.require(:period).permit(:start_date, :end_date, :resolution, :working_day, :quantity)
+      params.require(:period).permit(:start_date, :end_date, :resolution, :working_day, :quantity, :employee_id)
     end
 end
