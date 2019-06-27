ad34f1dc1c4be685747e680a98e8cc39a26853fc
Francisco <franciscomuniozs@gmail.com>
2019-06-27 15:10:55 -0400

    Last update - Added new ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 # Replace 127.0.0.1 with your server's IP address!
-server '172.16.7.151', user: 'deploy', roles: %w{app db web}
+server '172.16.7.155', user: 'deploy', roles: %w{app db web}
 
 # server-based syntax
 # ======================
