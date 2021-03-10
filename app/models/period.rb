2cd3d03086597aed62599189b920fb4c11a25dcb
Francisco Muñoz <franciscomuniozs@gmail.com>
2021-03-10 14:56:34 -0300

    Last update - Added quantity and working day

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,5 @@
 class Period < ApplicationRecord
+  extend Enumerize
   belongs_to :employee
   belongs_to :project
 
@@ -6,4 +7,9 @@ class Period < ApplicationRecord
 
   has_many :permanences, inverse_of: :period
   accepts_nested_attributes_for :permanences, reject_if: :all_blank, allow_destroy: true
+
+  enumerize :working_day, in: [:mañana, :tarde]
+
+  enumerize :quantity, in: [ :'1', :'2', :'3', :'4', :'5', :'6', :'7' ]
+
 end
